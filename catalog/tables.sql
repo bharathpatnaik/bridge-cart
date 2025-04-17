19523b47e8576dd844bdf5e4d23bd4d225efb404
Bharath Raghupatruni <bubh.rara@gmail.com>
2025-04-17 21:59:11 +0530

    Feature tables

---
+++ catalog/tables.sql
@@ -0,0 +1,64 @@
+-- ===========================
+-- 1. RAW Layer
+-- ===========================
+CREATE TABLE IF NOT EXISTS raw_customers (
+  id SERIAL PRIMARY KEY,
+  customer_id VARCHAR(255),
+  name VARCHAR(255),
+  age INT,
+  income INT,
+  gender VARCHAR(1),
+  mobile VARCHAR(50),
+  purchase_date TIMESTAMP,
+  purchase_amount DECIMAL(10, 2),
+  inserted_at TIMESTAMP DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Kolkata'),
+  UNIQUE (customer_id, purchase_date)  -- uniqueness constraint
+);
+
+-- ===========================
+-- 2. SILVER Layer
+-- ===========================
+CREATE TABLE IF NOT EXISTS silver_customers (
+  id SERIAL PRIMARY KEY,
+  customer_id VARCHAR(255),
+  name VARCHAR(255),
+  age INT,
+  income INT,
+  gender INT, -- 0 or 1 after standardization
+  mobile VARCHAR(50),
+  purchase_date TIMESTAMP,
+  purchase_amount DECIMAL(10,2),
+  inserted_at TIMESTAMP DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Kolkata'),
+  UNIQUE (customer_id, purchase_date)
+);
+
+-- ===========================
+-- 3. GOLD Layer: Segmented Data
+-- ===========================
+CREATE TABLE IF NOT EXISTS gold_customers_segments (
+  id SERIAL PRIMARY KEY,
+  customer_id VARCHAR(255),
+  name VARCHAR(255),
+  age INT,
+  income INT,
+  gender INT,
+  purchase_date TIMESTAMP,
+  purchase_amount DECIMAL(10,2),
+  clv DECIMAL(10,2),
+  churn_flag INT,
+  segment INT,
+  inserted_at TIMESTAMP DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Kolkata'),
+  UNIQUE (customer_id, purchase_date)
+);
+
+-- ===========================
+-- 4. GOLD Layer: Segment Metrics
+-- ===========================
+CREATE TABLE IF NOT EXISTS gold_segment_metrics (
+  segment INT PRIMARY KEY,
+  avg_clv DECIMAL(10,2),
+  churn_rate DECIMAL(5,4),
+  aov DECIMAL(10,2),
+  segment_contribution DECIMAL(10,4),
+  inserted_at TIMESTAMP DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Kolkata')
+);
